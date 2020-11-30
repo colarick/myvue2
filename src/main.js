43d7244d7dbe06835772d3a5faf274572b967480
colarick <46126838+colarick@users.noreply.github.com>
2020-12-01 00:09:32 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from "./store";
-import router from "./router";
+import router from "./router"
 Vue.config.productionTip = false
 //maincommit 1
-// main commit 2
-Vue.config.productionTip = false;
-
-//main commit 3
-//main 4
-
-
-//test2local
-//test2origin
-
-
 new Vue({
   el: "#app",
   store,
