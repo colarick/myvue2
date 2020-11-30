cfbe93c7741261a9f6fc3c5352187f25cb2bd8f2
colarick <46126838+colarick@users.noreply.github.com>
2020-12-01 00:09:32 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,7 @@ import App from './App.vue'
 import store from "./store";
 import router from "./router"
 Vue.config.productionTip = false
-//testlocal
-//testorigin
-
-
-//test2local
-//test2origin
-
-
-//test3local
+//maincommit 1
 new Vue({
   el: "#app",
   store,
