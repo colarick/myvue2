3679498b6db999e993ebc940a553410a6afa440a
colarick <46126838+colarick@users.noreply.github.com>
2020-12-01 02:32:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from "./store";
 import router from "./router";
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 
 
